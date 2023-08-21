59d78b5aa2d10197029acf558a89e5fc80682085
Siwakorn Pasawang <pasawang.sivakorn@gmail.com>
2023-08-21 12:00:00 +0700

    Create Homepage

---
+++ src/main/java/ku/cs/kafe/controller/HomeController.java
@@ -0,0 +1,15 @@
+package ku.cs.kafe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // return .html
+public class HomeController {
+    @RequestMapping("/")
+    public String getHomePage(Model model){
+        model.addAttribute("greeting","Sawaddee");
+
+        return "homepage";
+    }
+}
