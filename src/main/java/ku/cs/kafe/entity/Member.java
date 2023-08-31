d805360e8dac645ae60833e9ff08130e64dfd756
Siwakorn Pasawang <pasawang.sivakorn@gmail.com>
2023-08-31 20:53:51 +0700

    Edit member

--- src/main/java/ku/cs/kafe/entity/Member.java
+++ src/main/java/ku/cs/kafe/entity/Member.java
@@ -19,36 +19,4 @@ public class Member {
     private String password;
     private String name;
     private String role;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
